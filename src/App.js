55cc170276626b4d5983da5a04f2e9c856842738
Tanuj52 <rangatanuj5110@gmail.com>
2024-11-01 21:02:38 +0530

    Changed the Navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 
 export default class App extends Component {
-  c = "Jaaanu meenu baby rucha";
   render() {
     return (
       <div>
         <Navbar />
-        <News pageSize={10} />
+        <News pageSize={10} country='in' />
       </div>
     )
   }
